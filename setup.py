7e1f7d8f691a29d4363cfb31169c4df7222c1440
Laurens Van Houtven <_@lvh.cc>
2012-09-27 17:18:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(name='txeasymail',
+      version='20120927',
+      description='Twisted client for easy email sending',
+      url='https://github.com/lvh/txeasymail',
+
+      author='Laurens Van Houtven',
+      author_email='_@lvh.cc',
+
+      packages=find_packages(),
+
+      install_requires=['twisted'],
+
+      license='ISC',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Twisted",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        ])
