9ff12654463bd9103e9068d0e6298e1d2501c264
Laurens Van Houtven <_@lvh.cc>
2012-10-09 20:52:44 +0200

    Support unicode address headers better

--- txeasymail/mime.py
+++ txeasymail/mime.py
@@ -1,5 +1,6 @@
 from email import Charset, generator, header
 from email.mime import multipart, text
+from email.utils import formataddr, parseaddr
 
 try:
     from cStringIO import StringIO
@@ -12,14 +13,22 @@ Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
 
 
 def buildMessage(headers, parts):
+    """
+    Builds a message from some headers and MIME parts.
+    """
     message = multipart.MIMEMultipart('alternative')
 
     for name, value in headers.iteritems():
-        multipart[name] = _encodeHeader(value)
+        if name == "From":
+            multipart[name] = _encodeAddresses(value)
+        elif name in ["To", "Cc", "Bcc"]:
+            multipart[name] = _encodeAddress(value)
+        else:
+            multipart[name] = _encodeHeader(value)
 
     for partType, part in parts.iteritems():
-        mt = text.MIMEText(part.encode("utf-8"), partType, "UTF-8").encode()
-        message.attach(mt)
+        mimeText = text.MIMEText(part.encode("utf-8"), partType, "UTF-8")
+        message.attach(mimeText.encode())
 
     return message
 
@@ -35,6 +44,29 @@ def _encodeHeader(headerValue):
     except UnicodeError:
         encoded = headerValue.encode("utf-8")
         return header.Header(encoded, "UTF-8").encode()
+
+
+def _encodeAddress(address):
+    """
+    Encodes an address.
+
+    This parses the address into the real name and the actual address. Each
+    part is then encoded as per ``_encodeHeader``.
+    """
+    return formataddr(map(_encodeHeader, parseaddr(address)))
+
+
+COMMASPACE = ", "
+
+
+def _encodeAddresses(addresses):
+    """
+    Encodes multiple e-mail addresses.
+
+    Splits on a comma followed by a space to produce individual addresses.
+    Each such address is then encoded as per ``_encodeAddress``.
+    """
+    return COMMASPACE.join(map(_encodeAddress, addresses.split(COMMASPACE)))
 
 
 def messageToFile(message):
